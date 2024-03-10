af2e229ef1282d96b99df6acf6a598887100ede3
Isadora <raspberryabel@gmail.com>
2024-03-11 01:02:55 +0300

    added one more algorithm in task4

---
+++ les1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 61;
+int b = 75;
+int c = 94;
+int d = 11;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
