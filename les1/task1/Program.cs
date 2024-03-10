d6b7b25f9b1b29ebf7e7ec05541e0d5ebc01470f
Isadora <raspberryabel@gmail.com>
2024-03-10 21:16:12 +0300

    ааааааааааааааааааааааа

--- les1/task1/Program.cs
+++ les1/task1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine(value);
--- les1/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ les1/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43fd72605095a022c0b9b6dbe32b3328e11cddcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88b87dc89c334744161db1ceeb54c76343318e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
