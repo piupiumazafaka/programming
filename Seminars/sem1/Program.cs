179a03ecafd3445c4dac533a3aa150195273bc32
Isadora <raspberryabel@gmail.com>
2024-03-11 02:22:28 +0300

    added new seminar1 file

---
+++ Seminars/sem1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("What is your name?");
+string name = Console.ReadLine();
+System.Console.WriteLine("Hello, " + name + "!");
+System.Console.WriteLine("How old are you?");
+
+int age = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(age * age);
