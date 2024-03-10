f2dd7345a76493da6c12d92a88d3f57d2ec4dbfb
Isadora <raspberryabel@gmail.com>
2024-03-10 21:36:27 +0300

    added task2 file

--- les1/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ les1/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88b87dc89c334744161db1ceeb54c76343318e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b7b25f9b1b29ebf7e7ec05541e0d5ebc01470f")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
