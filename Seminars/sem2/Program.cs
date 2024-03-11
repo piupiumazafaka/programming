52f7db170ff6ee9665f022dc75c582a5558a8882
Isadora <raspberryabel@gmail.com>
2024-03-11 14:16:53 +0300

    added two more tasks

--- Seminars/sem2/Program.cs
+++ Seminars/sem2/Program.cs
@@ -22,17 +22,66 @@
 // 254 => 5^4 = 625
 // 617 => 1
 
-int num = new Random().Next(100, 1000);  // 487
+// int num = new Random().Next(100, 1000);  // 487
+// Console.WriteLine(num);
+
+// int num2 = num/10 % 10;   // 487 /10= 48   48%10 = 8
+// int num3= num%10;
+// int result=1;
+
+// while(num3>0)
+// {
+//     result = result*num2;
+//     num3= num3-1;
+// }
+
+// Console.WriteLine(result);
+
+// Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число некратно
+// второму, то программа выводит остаток от деления.
+// Примеры
+// 14, 5 => нет, 4
+// 16, 8 => да
+// 4, 3 => нет, 1
+
+// Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());   // 14
+
+// Console.WriteLine("Введите число");
+// int num2 = Convert.ToInt32(Console.ReadLine()); // 5
+
+// if(num1 % num2 == 0)     //    14 % 5 = 4  -
+// {
+//     Console.WriteLine("Да");
+// }
+// else                   //  +
+// {
+//     Console.WriteLine("Нет, "+ num1 %num2); // Нет, 4
+// }
+
+
+// Напишите программу, которая выводит третью с
+// конца цифру заданного числа или сообщает, что
+// третьей цифры нет.
+// 456 => 6
+// 7812 => 8
+// 91 => Третьей цифры нет
+
+int num = new Random().Next(1, 10000);  // 4879
 Console.WriteLine(num);
 
-int num2 = num/10 % 10;   // 487 /10= 48   48%10 = 8
-int num3= num%10;
-int result=1;
-
-while(num3>0)
+if(num<100) // -
 {
-    result = result*num2;
-    num3= num3-1;
+    Console.WriteLine("Третьей цифры нет");
 }
+else      // +
+{
+    while(num>=1000) // +
+    {
+        num = num/10;
+    }
 
-Console.WriteLine(result);
+    Console.WriteLine(num%10); // 487 %10 = 7
+}
