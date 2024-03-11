27cba90045ea33d0998dccfe927d447938a51ad4
Isadora <raspberryabel@gmail.com>
2024-03-11 03:04:12 +0300

    added one more example for sem1

---
+++ Seminars/sem1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+    System.Console.WriteLine("True");
+}
+else
+{
+    System.Console.WriteLine("False");
+}
