6f5fd217156acb5bb3f28594dbdc9f275b3e2216
Isadora <raspberryabel@gmail.com>
2024-03-11 03:50:57 +0300

    one more task

--- Seminars/sem1_1/Program.cs
+++ Seminars/sem1_1/Program.cs
@@ -1,13 +1,22 @@
-﻿System.Console.Write("Input first number: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Input second number: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+﻿// System.Console.Write("Input first number: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number2 * number2 == number1)
-{
-    System.Console.WriteLine("True");
-}
-else
-{
-    System.Console.WriteLine("False");
-}
+// if (number2 * number2 == number1)
+// {
+//     System.Console.WriteLine("True");
+// }
+// else
+// {
+//     System.Console.WriteLine("False");
+// }
+
+// System.Console.Write("Input N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int start = -n;
+// while(start <= n)
+// {
+//     System.Console.Write(start + " ");
+//     start++;
+// }
